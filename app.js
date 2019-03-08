3b6ec0673faf8854b3f52dc9813e2e7d15278ea1
Riley Lu <luruiyi1988@hotmail.com>
2019-03-08 17:39:22 -0500

    update

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ function onReady() {
     const addToDoForm = document.getElementById('addToDoForm');
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
-    addToDoForm.addEventListener('submit', event => {
-      event.preventDefault();
 
+
+    addToDoForm.addEventListener('submit', event => {
+
+      event.preventDefault();
       //get the text
       let title  = newToDoText.value;
 
       //creat a new li
       let newLi = document.createElement('li');
 
+      //set the title
+      newLi.textContent = title;
+
       //create a new input
       let checkbox = document.createElement('input');
 
       //set the input's type to checkbox
       checkbox.type = "checkbox";
-
-      //set the title
-      newLi.textContent = title;
 
       //attach the checkbox to the li
       newLi.appendChild(checkbox);
@@ -28,10 +30,27 @@ function onReady() {
 
       //empty the input
       newToDoText.value = '';
+
+      let btn = document.createElement("button");
+      btn.innerHTML = "Delete";
+    btn.classList.add("mdl-button");
+
+      newLi.appendChild(btn);
+
+
+      btn.addEventListener('click', event => {
+        event.preventDefault();
+        if(checkbox.checked) {
+          newLi.remove();
+        }
+   });
+
     });
- }
+
+
+};
 
 window.onload = function() {
-  alert("The window has loaded!");
+
    onReady();
 };
